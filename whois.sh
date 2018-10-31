77292ff92f32d9d3a45eff66fabd3e1c3886fbdc
Rishi <homkafle@gmail.com>
2018-10-31 14:10:40 -0500

    Update whois.sh

--- whois.sh
+++ whois.sh
@@ -17,110 +17,109 @@
                              \|                              
 ********************************************************************
 "
-
 echo "Fetching Registrar server record ..."
 var=`whois $1 | grep 'Registrar WHOIS Server:' | head -n1 | awk '{print $4'}`
 
 if [ $var ]; then
-	echo "Registrar server found:"$var
-	echo "Fetching Records from found registrar server "$var " ..."
-	record=$(whois -h $var $1)
+        echo "Registrar server found:"$var
+        echo "Fetching Records from found registrar server "$var " ..."
+        record=$(whois -h $var $1)
 else
-	echo "Fetching Records from the default whois server..."
-	record=$(whois $1)
+        echo "Fetching Records from the default whois server..."
+        record=$(whois $1)
 fi
 
-echo "$record" > /tmp/tmp.tmp
+echo "$record" > /tmp/$USER.tmp
 
 #echo "$record" This will display all the record
 
 echo ""
 echo "-----------------------DOMAIN INFORMATION-------------------"
 
-domainname=$(grep 'Domain Name:' /tmp/tmp.tmp| cut -d: -f2-)
+domainname=$(grep 'Domain Name:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Domain :" $domainname
-registrar=$(grep 'Registrar:' /tmp/tmp.tmp| cut -d: -f2-)
+registrar=$(grep 'Registrar:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Registrar :" $registrar
-registeredon=$(grep 'Creation Date:' /tmp/tmp.tmp| cut -d: -f2-)
+registeredon=$(grep 'Creation Date:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Registerd On :" $registeredon
-expireson=$(grep 'Registrar Registration Expiration Date:' /tmp/tmp.tmp| cut -d: -f2-)
+expireson=$(grep 'Registrar Registration Expiration Date:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Expires On :" $expireson
-updatedon=$(grep 'Updated Date:' /tmp/tmp.tmp| cut -d: -f2-)
+updatedon=$(grep 'Updated Date:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Updated On :" $updatedon
-status=$(grep 'Domain Status:' /tmp/tmp.tmp| cut -d: -f2-)
+status=$(grep 'Domain Status:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Status :"
 echo "$status"
-nameservers=$(grep 'Name Server:' /tmp/tmp.tmp| cut -d: -f2-)
+nameservers=$(grep 'Name Server:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Name Servers :"
 echo "$nameservers"
 echo ""
 echo "-----------------------REGISTRANT CONTACT-------------------"
 
-rname=$(grep 'Registrant Name:' /tmp/tmp.tmp| cut -d: -f2-)
+rname=$(grep 'Registrant Name:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Name :" $rname
-rorg=$(grep 'Registrant Organization:' /tmp/tmp.tmp| cut -d: -f2-)
+rorg=$(grep 'Registrant Organization:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Organization :" $rorg
-rstreet=$(grep 'Registrant Street:' /tmp/tmp.tmp| cut -d: -f2-)
+rstreet=$(grep 'Registrant Street:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Street :" $rstreet
-rcity=$(grep 'Registrant City:' /tmp/tmp.tmp| cut -d: -f2-)
+rcity=$(grep 'Registrant City:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "City :" $rcity
-rstate=$(grep 'Registrant State/Province:' /tmp/tmp.tmp| cut -d: -f2-)
+rstate=$(grep 'Registrant State/Province:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "State :" $rstate
-rcode=$(grep 'Registrant Postal Code:' /tmp/tmp.tmp| cut -d: -f2-)
+rcode=$(grep 'Registrant Postal Code:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Postal Code :" $rcode
-rcountry=$(grep 'Registrant Country:' /tmp/tmp.tmp| cut -d: -f2-)
+rcountry=$(grep 'Registrant Country:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Country :" $rcountry
-rphone=$(grep 'Registrant Phone:' /tmp/tmp.tmp| cut -d: -f2-)
+rphone=$(grep 'Registrant Phone:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Phone :" $rphone
-rfax=$(grep 'Registrant Fax::' /tmp/tmp.tmp| cut -d: -f2-)
+rfax=$(grep 'Registrant Fax::' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Fax :" $rfax
-remail=$(grep 'Registrant Email:' /tmp/tmp.tmp| cut -d: -f2-)
+remail=$(grep 'Registrant Email:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Email :" $remail
 echo ""
 echo "-----------------------ADMINISTRATIVE CONTACT---------------"
 
-aname=$(grep 'Registrant Name:' /tmp/tmp.tmp| cut -d: -f2-)
+aname=$(grep 'Registrant Name:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Name :" $aname
-aorg=$(grep 'Admin Organization:' /tmp/tmp.tmp| cut -d: -f2-)
+aorg=$(grep 'Admin Organization:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Organization :" $aorg
-astreet=$(grep 'Admin Street:' /tmp/tmp.tmp| cut -d: -f2-)
+astreet=$(grep 'Admin Street:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Street :" $astreet
-acity=$(grep 'Admin City:' /tmp/tmp.tmp| cut -d: -f2-)
+acity=$(grep 'Admin City:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "City :" $acity
-astate=$(grep 'Admin State/Province:' /tmp/tmp.tmp| cut -d: -f2-)
+astate=$(grep 'Admin State/Province:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "State :" $astate
-acode=$(grep 'Admin Postal Code:' /tmp/tmp.tmp| cut -d: -f2-)
+acode=$(grep 'Admin Postal Code:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Postal Code :" $acode
-acountry=$(grep 'Admin Country:' /tmp/tmp.tmp| cut -d: -f2-)
+acountry=$(grep 'Admin Country:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Country :" $acountry
-aphone=$(grep 'Admin Phone:' /tmp/tmp.tmp| cut -d: -f2-)
+aphone=$(grep 'Admin Phone:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Phone :" $aphone
-afax=$(grep 'Admin Fax::' /tmp/tmp.tmp| cut -d: -f2-)
+afax=$(grep 'Admin Fax::' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Fax :" $afax
-aemail=$(grep 'Admin Email:' /tmp/tmp.tmp| cut -d: -f2-)
+aemail=$(grep 'Admin Email:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Email :" $aemail
 echo ""
 echo "-----------------------TECHNICAL CONTACT--------------------"
 
-tname=$(grep 'Tech Name:' /tmp/tmp.tmp| cut -d: -f2-)
+tname=$(grep 'Tech Name:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Name :" $tname
-torg=$(grep 'Tech Organization:' /tmp/tmp.tmp| cut -d: -f2-)
+torg=$(grep 'Tech Organization:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Organization :" $torg
-tstreet=$(grep 'Tech Street:' /tmp/tmp.tmp| cut -d: -f2-)
+tstreet=$(grep 'Tech Street:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Street :" $astreet
-tcity=$(grep 'Tech City:' /tmp/tmp.tmp| cut -d: -f2-)
+tcity=$(grep 'Tech City:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "City :" $acity
-tstate=$(grep 'Tech State/Province:' /tmp/tmp.tmp| cut -d: -f2-)
+tstate=$(grep 'Tech State/Province:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "State :" $tstate
-tcode=$(grep 'Tech Postal Code:' /tmp/tmp.tmp| cut -d: -f2-)
+tcode=$(grep 'Tech Postal Code:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Postal Code :" $tcode
-tcountry=$(grep 'Tech Country:' /tmp/tmp.tmp| cut -d: -f2-)
+tcountry=$(grep 'Tech Country:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Country :" $tacountry
-tphone=$(grep 'Tech Phone:' /tmp/tmp.tmp| cut -d: -f2-)
+tphone=$(grep 'Tech Phone:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Phone :" $tphone
-tfax=$(grep 'Tech Fax::' /tmp/tmp.tmp| cut -d: -f2-)
+tfax=$(grep 'Tech Fax::' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Fax :" $tfax
-temail=$(grep 'Tech Email:' /tmp/tmp.tmp| cut -d: -f2-)
+temail=$(grep 'Tech Email:' /tmp/$USER.tmp| cut -d: -f2-)
 echo "Email :" $temail
 echo "------------------------------------------------------------"
 
